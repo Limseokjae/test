a51369b75538972c37a3ed07a07492801e1c6195
somi.chae <somi.chae@lotte.net>
2023-05-09 06:34:55 +0000

    remove print

--- crawler/youtube/youtube.py
+++ crawler/youtube/youtube.py
@@ -10,7 +10,7 @@ def main():
     parser.add_argument('-video', '--save_video', action='store_true')
     parser.add_argument('-meta', '--save_meta', action='store_true')
     args = parser.parse_args()
-    data = pd.read_excel('/home/work/som2/R_test/data_urls_1000.xlsx')
+    data = pd.read_excel('data_urls_1000.xlsx')
     done = set()
     error = set()
     pbar = tqdm(range(len(data)))
@@ -19,7 +19,6 @@ def main():
     save_meta_dir = 'butter_meta'  # 영상 download할 디렉토리
     os.makedirs(save_video_dir, exist_ok=True)
     os.makedirs(save_meta_dir, exist_ok=True)
-    print('=======')
     for i in pbar:
         url = data.iloc[i].url
         if url not in done:  # 진행하지 않은 url만 download 시도
@@ -36,8 +35,7 @@ def main():
 
             done.add(url)  # 진행한 url 체크 
         pbar.set_description(f'{len(done)}')
-        if i:
-            break
+
     print(f'Done!!! -> {len(done)}')
     print(f'Error!!! -> {len(error)}')
 
